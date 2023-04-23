82bb97264cc4c86e160a4f036e0e8f2f811cbaa5
ROSELYN0208 <roselymothapomr@gmail.com>
2023-04-23 11:37:04 -0700

    additional commit 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("Size a of char :%d byte(s)\n",sizeof(char));
-	printf("Size a of int :%d byte(s)\n",sizeof(int));
-	printf("Size a of long int :%d byte(s)\n",sizeof(long int));
-	printf("Size a of long long int :%d byte(s)\n",sizeof(long long int));
-	printf("Size a of float :%d byte(s)\n",sizeof(float));
-    return (0);
+printf("Size a of char :%d byte(s)\n",sizeof(char));
+printf("Size a of int :%d byte(s)\n",sizeof(int));
+printf("Size a of long int :%d byte(s)\n",sizeof(long int));
+printf("Size a of long long int :%d byte(s)\n",sizeof(long long int));
+printf("Size a of float :%d byte(s)\n",sizeof(float));
+return (0);
 }
